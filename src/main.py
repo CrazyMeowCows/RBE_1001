ae92cebfa35bbd1aecf321c349133f49bba99024
Tommy R <drumming.dog101@gmail.com>
2024-10-28 15:25:50 -0400

    Working, figured out PERCENT bs

--- src/main.py
+++ src/main.py
@@ -11,15 +11,6 @@ WHEEL_BASE_MM = 270
 # Variable Setup
 brain = Brain()
 controller = Controller()
-timer = Timer()
-acc = 0
-
-# State Machine Definitions
-IDLE = "idle"
-DRIVING = "driving"
-ARM_MOVING_UP = "arm_moving_up"
-ARM_MOVING_DOWN = "arm_moving_down"
-state = IDLE
 
 def set_state(new_state):
     global state
@@ -30,7 +21,7 @@ def set_state(new_state):
 # Motor and Sensor Definitions
 left_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True)
 right_motor = Motor(Ports.PORT10, GearSetting.RATIO_18_1, False)
-arm_motor = Motor(Ports.PORT8, GearSetting.RATIO_18_1, False)
+arm_motor = Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)
 sonar = Sonar(brain.three_wire_port.e)
 bumper = Bumper(brain.three_wire_port.d)
 
@@ -47,22 +38,17 @@ arm_motor.set_stopping(BrakeType.HOLD)
 
 # Function Definitions 
 def sonar_detected(dist_thresh_mm):
-    global acc
-    dist_mm = sonar.distance(MM)
-    if (dist_mm <= dist_thresh_mm and dist_mm > 0):
-        acc = acc+1
-    else:
-        acc = max(acc-1, 0)
-    return acc > 50
+    sleep(20)
+    return sonar.distance(MM) < dist_thresh_mm
 
 def bumper_pressed():
-    driveTrain.drive(FORWARD, 100, PERCENT)
+    driveTrain.drive(FORWARD, 100*DRIVE_GEAR_RATIO, VelocityUnits.PERCENT)
     while (not sonar_detected(75)):
-        sleep(20)
+        pass
     driveTrain.stop()
     arm_motor.spin_to_position(20*ARM_GEAR_RATIO, DEGREES, 100, VelocityUnits.PERCENT, True)
     sleep(5000)
-    arm_motor.spin_to_position(0*ARM_GEAR_RATIO, DEGREES, 100, VelocityUnits.PERCENT, False)
+    arm_motor.spin_to_position(0*ARM_GEAR_RATIO, DEGREES, 20, VelocityUnits.PERCENT, False)
 
 
 # Button Bindings
