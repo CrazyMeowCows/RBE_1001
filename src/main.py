8bca4fd23ea2442dac9175162e16aefc889230a6
Tommy R <drumming.dog101@gmail.com>
2024-11-12 12:14:01 -0500

    Created basic teleop code

--- src/main.py
+++ src/main.py
@@ -9,11 +9,6 @@ WHEEL_RAD_MM = 50.8
 TRACK_WIDTH_MM = 285
 WHEEL_BASE_MM = 205
 
-MIN_REFLECTIVITY = 166
-MAX_REFLECTIVITY = 2645
-LINE_FOLLOWING_GAIN = 0.6
-GYRO_GAIN = 0.165
-
 
 # Variable Setup ----------------------------------------------------
 brain = Brain()
@@ -37,18 +32,15 @@ controller = Controller()
 # Motor and Sensor Definitions --------------------------------------
 left_motor = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
 right_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-arm_motor = Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)
-gyro = Inertial(Ports.PORT20)
-
-sonar = Sonar(brain.three_wire_port.e)
-left_line = Line(brain.three_wire_port.h)
-right_line = Line(brain.three_wire_port.b)
+center_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False) #TODO: SET PORTS
+effector_motor = Motor(Ports.PORT8, GearSetting.RATIO_18_1, True) #TODO: SET PORTS
 
 
 # Motor and Sensor Setup --------------------------------------------
 left_motor.reset_position()
 right_motor.reset_position()
-arm_motor.reset_position()
+center_motor.reset_position()
+effector_motor.reset_position()
 
 driveTrain = DriveTrain(left_motor, right_motor, 2*math.pi*WHEEL_RAD_MM, TRACK_WIDTH_MM, WHEEL_BASE_MM, MM, DRIVE_GEAR_RATIO)
 # Use with gear ratio set to 1/5
@@ -57,68 +49,28 @@ driveTrain = DriveTrain(left_motor, right_motor, 2*math.pi*WHEEL_RAD_MM, TRACK_W
 # driveTrain.drive_for(FORWARD, 20, DistanceUnits.CM, 50*DRIVE_GEAR_RATIO, VelocityUnits.PERCENT, False)
 
 driveTrain.set_stopping(BrakeType.HOLD)
-arm_motor.set_stopping(BrakeType.HOLD)
+center_motor.set_stopping(BrakeType.HOLD)
+effector_motor.set_stopping(BrakeType.HOLD)
 
 
 # Function Definitions ----------------------------------------------
-# Scale a value from a min->max to 0->1
-def scale(val, min, max):
-    return (val-min)/(max-min)
-
-# Get continous angle error from discontinuous angles
-def angle_error_deg(target, current):
-    diff = math.radians(target) - math.radians(current)
-    return math.degrees(math.atan2(math.sin(diff), math.cos(diff)))
-
-# Line follow at a given speed until the ultrasonic detects something
-def line_follow_to_wall(dist_to_wall_mm, speed_percent):
-    while (sonar.distance(MM) > dist_to_wall_mm):
-        errorL = scale(left_line.value(), MIN_REFLECTIVITY, MAX_REFLECTIVITY)
-        errorR = -scale(right_line.value(), MIN_REFLECTIVITY, MAX_REFLECTIVITY)
-        sum_error = (errorL + errorR) * LINE_FOLLOWING_GAIN * speed_percent
-
-        left_motor.spin(FORWARD, speed_percent + sum_error, PERCENT)
-        right_motor.spin(FORWARD, speed_percent - sum_error, PERCENT)
-        sleep(20)
-
-    driveTrain.stop()
-
-# Drive forward while maintaing the given target rotation
-def gyro_drive_to_wall(target_rot_deg, dist_to_wall_mm, speed_percent):
-    current = gyro.heading()
-
-    while (sonar.distance(MM) > dist_to_wall_mm):
-        current = gyro.heading()
-        error = angle_error_deg(target_rot_deg, current)*GYRO_GAIN*speed_percent
-
-        left_motor.spin(FORWARD, speed_percent + error, PERCENT)
-        right_motor.spin(FORWARD, speed_percent - error, PERCENT)
-        sleep(20)
-
-    driveTrain.stop()
-
-# Turn to a given gyro heading
-def gyro_turn(target_rot_deg, dir, speed_percent):
-    target = target_rot_deg
-    driveTrain.turn(dir, speed_percent*DRIVE_GEAR_RATIO, PERCENT)
-    current = gyro.heading()
-
-    while (abs(angle_error_deg(target, current)) > 1):
-        current = gyro.heading()
-        sleep(20)
-
-    driveTrain.stop()
 
 
-# Button Bindings ---------------------------------------------------
-def start_routine():
-    gyro.calibrate()
-    while gyro.is_calibrating():
-        sleep(50)
+# Main Loop  --------------------------------------------------------
+while True:
+    forward = controller.axis3.position()
+    sideways = controller.axis4.position()
+    rotation = controller.axis1.position()
+
+    left_motor.spin(FORWARD, forward + rotation, PERCENT)
+    right_motor.spin(FORWARD, forward - rotation, PERCENT)
+    center_motor.spin(FORWARD, sideways, PERCENT)
+
+    rButton = 0
+    if (controller.buttonR1.pressing()):
+        rButton += 100
+    if (controller.buttonR2.pressing()):
+        rButton += -100
+
+    effector_motor.spin(FORWARD, rButton, PERCENT)
     
-    gyro_drive_to_wall(0, 200, 100)
-    gyro_turn(180, LEFT, 50)
-    gyro_drive_to_wall(180, 200, 100)
-    gyro_turn(0, RIGHT, 50)
-
-controller.buttonB.pressed(start_routine)
