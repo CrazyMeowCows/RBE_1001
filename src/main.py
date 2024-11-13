a90f71b8ab919ef79b410c78946b13a935b16116
Tommy R <drumming.dog101@gmail.com>
2024-11-13 16:49:57 -0500

    removed drivetrain, boolean multiplication

--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@
 from vex import *
 import math
 
+
 # Define Constants --------------------------------------------------
 DRIVE_GEAR_RATIO = 1/5
 ARM_GEAR_RATIO = 1/5
 WHEEL_RAD_MM = 50.8
 TRACK_WIDTH_MM = 285
 WHEEL_BASE_MM = 205
+
 
 # Variable Setup ----------------------------------------------------
 brain = Brain()
@@ -34,10 +36,10 @@ right_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
 center_motor = Motor(Ports.PORT4, GearSetting.RATIO_18_1, False)
 effector_motor = Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)
 
-Vision3__LEMON = Signature (3, 1335, 1737, 1536, -3855, -3589, -3722, 2.6, 0)
-Vision3__LIME = Signature (2, -6813, -5985, -6400, -3439, -2829, -3134, 3.4, 0)
-Vision3__ORANGE = Signature (1, 5939, 6607, 6273, -2463, -2145, -2304, 1.7, 0)
-Vision3 = Vision (Ports.PORT3, 24, Vision3__LEMON, Vision3__LIME, Vision3__ORANGE)
+Vision3__LEMON = Signature(3, 1335, 1737, 1536, -3855, -3589, -3722, 2.6, 0)
+Vision3__LIME = Signature(2, -6813, -5985, -6400, -3439, -2829, -3134, 3.4, 0)
+Vision3__ORANGE = Signature(1, 5939, 6607, 6273, -2463, -2145, -2304, 1.7, 0)
+Vision3 = Vision(Ports.PORT3, 24, Vision3__LEMON, Vision3__LIME, Vision3__ORANGE)
 
 
 # Motor and Sensor Setup --------------------------------------------
@@ -46,13 +48,8 @@ right_motor.reset_position()
 center_motor.reset_position()
 effector_motor.reset_position()
 
-driveTrain = DriveTrain(left_motor, right_motor, 2*math.pi*WHEEL_RAD_MM, TRACK_WIDTH_MM, WHEEL_BASE_MM, MM, DRIVE_GEAR_RATIO)
-# Use with gear ratio set to 1/5
-# driveTrain.drive(FORWARD, 100*DRIVE_GEAR_RATIO, PERCENT)
-# driveTrain.drive(FORWARD, 30, RPM)
-# driveTrain.drive_for(FORWARD, 20, DistanceUnits.CM, 50*DRIVE_GEAR_RATIO, VelocityUnits.PERCENT, False)
-
-driveTrain.set_stopping(BrakeType.HOLD)
+left_motor.set_stopping(BrakeType.HOLD)
+right_motor.set_stopping(BrakeType.HOLD)
 center_motor.set_stopping(BrakeType.HOLD)
 effector_motor.set_stopping(BrakeType.HOLD)
 
@@ -68,22 +65,18 @@ def find_fruit():
 
 controller.buttonB.pressed(find_fruit)
 
+
 # Main Loop  --------------------------------------------------------
 while True:
     forward = controller.axis3.position()
     sideways = controller.axis4.position()
     rotation = controller.axis1.position()
+    rButton = controller.buttonR1.pressing()*100 - controller.buttonR2.pressing()*100
 
     left_motor.spin(FORWARD, forward + rotation, PERCENT)
     right_motor.spin(FORWARD, forward - rotation, PERCENT)
-    center_motor.spin(FORWARD, sideways, PERCENT)
-
-    rButton = 0
-    if (controller.buttonR1.pressing()):
-        rButton += 100
-    if (controller.buttonR2.pressing()):
-        rButton += -100
-
+    center_motor.spin(FORWARD, sideways * DRIVE_GEAR_RATIO, PERCENT)
     effector_motor.spin(FORWARD, rButton, PERCENT)
+
     sleep(20)
     
