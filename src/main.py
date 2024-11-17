6427ab022363049759814d8a8ad1e60cbc10c227
Tommy R <drumming.dog101@gmail.com>
2024-11-17 15:57:22 -0500

    FINDS LIMES ONLY :cry:

--- src/main.py
+++ src/main.py
@@ -39,10 +39,10 @@ right_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
 center_motor = Motor(Ports.PORT4, GearSetting.RATIO_18_1, False)
 effector_motor = Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)
 
-Vision3__LEMON = Signature(3, 1335, 1737, 1536, -3855, -3589, -3722, 2.6, 0)
-Vision3__LIME = Signature(2, -6813, -5985, -6400, -3439, -2829, -3134, 3.4, 0)
-Vision3__ORANGE = Signature(1, 5939, 6607, 6273, -2463, -2145, -2304, 1.7, 0)
-Vision3 = Vision(Ports.PORT3, 24, Vision3__LEMON, Vision3__LIME, Vision3__ORANGE)
+Vision3__ORANGE = Signature (1, 6149, 6747, 6448, -2375, -2061, -2218, 3.9, 0)
+Vision3__LIME = Signature (2, -6089, -4819, -5454, -4097, -3219, -3658, 5.1, 0)
+Vision3__LEMON = Signature (3, 1935, 2783, 2358, -3277, -2701, -2990, 3, 0)
+Vision3 = Vision(Ports.PORT3, 50, Vision3__LEMON, Vision3__LIME, Vision3__ORANGE)
 
 
 # Motor and Sensor Setup --------------------------------------------
@@ -62,36 +62,37 @@ def find_fruit():
     acc = 20
 
     while acc > 0:
-        largest_object = Vision3.largest_object()
-        if largest_object.exists:
+        
+        if Vision3.take_snapshot(Vision3__LIME) or Vision3.take_snapshot(Vision3__LEMON) or Vision3.take_snapshot(Vision3__ORANGE):
+            largest_object = Vision3.largest_object()
             acc = 20
+            x_error = (largest_object.centerX - RESOLUTION_WIDTH/2)*GAIN_X #right is +
+            y_error = (largest_object.centerY - RESOLUTION_HEIGHT/2)*GAIN_Y #down is +
+
+            left_motor.spin(REVERSE, 20 - x_error, PERCENT)
+            right_motor.spin(REVERSE, 20 + x_error, PERCENT)
+            effector_motor.spin(REVERSE, y_error, PERCENT)
+            print(y_error)
         else:
+            print("No Fruit Found")
             acc = max(acc-1, 0)
 
-        x_error = (largest_object.centerX - RESOLUTION_WIDTH/2)*GAIN_X #right is +
-        y_error = (largest_object.centerY - RESOLUTION_HEIGHT/2)*GAIN_Y #down is +
-
-        left_motor.spin(FORWARD, 20 + x_error, PERCENT)
-        right_motor.spin(FORWARD, 20 - x_error, PERCENT)
-        effector_motor.spin(REVERSE, y_error, PERCENT)
-
         sleep(20)
-
 
 controller.buttonB.pressed(find_fruit)
 
 
 # Main Loop  --------------------------------------------------------
-while True:
-    forward = controller.axis3.position()
-    sideways = controller.axis4.position()
-    rotation = controller.axis1.position()
-    rButton = (controller.buttonR1.pressing()-controller.buttonR2.pressing())*100
+# while True:
+#     forward = controller.axis3.position()
+#     sideways = controller.axis4.position()
+#     rotation = controller.axis1.position()
+#     rButton = (controller.buttonR1.pressing()-controller.buttonR2.pressing())*100
 
-    left_motor.spin(FORWARD, forward + rotation, PERCENT)
-    right_motor.spin(FORWARD, forward - rotation, PERCENT)
-    center_motor.spin(FORWARD, sideways * DRIVE_GEAR_RATIO, PERCENT)
-    effector_motor.spin(FORWARD, rButton, PERCENT)
+#     left_motor.spin(FORWARD, forward + rotation, PERCENT)
+#     right_motor.spin(FORWARD, forward - rotation, PERCENT)
+#     center_motor.spin(FORWARD, sideways * DRIVE_GEAR_RATIO, PERCENT)
+#     effector_motor.spin(FORWARD, rButton, PERCENT)
 
-    sleep(20)
+#     sleep(20)
     
