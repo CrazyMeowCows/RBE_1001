6222ae8a4c03cd97630a987be055b05b18323bf3
Tommy R <drumming.dog101@gmail.com>
2024-12-06 17:34:20 -0500

    Tuning fork

--- src/main.py
+++ src/main.py
@@ -11,13 +11,12 @@ TRACK_WIDTH_MM = 285
 WHEEL_BASE_MM = 205
 RESOLUTION_WIDTH = 316
 RESOLUTION_HEIGHT = 210
-GAIN_X = 1
+GAIN_X = 0.3
 GAIN_Y = 1
 MIN_REFLECTIVITY = 166
 MAX_REFLECTIVITY = 2645
-LINE_FOLLOWING_GAIN = 30
-# GYRO_GAIN = 0.05
-GYRO_GAIN = 1.5
+LINE_FOLLOWING_GAIN = 0.6
+GYRO_GAIN = 0.05
 
 
 # Variable Setup ----------------------------------------------------
@@ -30,7 +29,7 @@ left_motor = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
 right_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
 center_motor = Motor(Ports.PORT4, GearSetting.RATIO_18_1, True)
 elbow_motor = Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)
-effector_motor = Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
+effector_motor = Motor(Ports.PORT5, GearSetting.RATIO_18_1, False)
 
 gyro = Inertial(Ports.PORT11)
 
@@ -38,10 +37,10 @@ left_line = Line(brain.three_wire_port.e)
 right_line = Line(brain.three_wire_port.f)
 sonar = Sonar(brain.three_wire_port.c)
 
-Vision3__LEMON = Signature(3, 1335, 1737, 1536, -3855, -3589, -3722, 2.6, 0)
-Vision3__LIME = Signature(2, -6813, -5985, -6400, -3439, -2829, -3134, 3.4, 0)
-Vision3__ORANGE = Signature(1, 5939, 6607, 6273, -2463, -2145, -2304, 1.7, 0)
-Vision3 = Vision(Ports.PORT20, 24, Vision3__LEMON, Vision3__LIME, Vision3__ORANGE)
+Vision5__ORANGE_FRUIT = Signature (1, 97, 2571, 1334, -3207, -2687, -2947, 2.6, 0)
+Vision5__LIME_FRUIT = Signature (2, -6089, -4819, -5454, -4097, -3219, -3658, 5.4, 0)
+Vision5__LEMON_FRUIT = Signature (3, 515, 2521, 1518, -3985, -3557, -3771, 3.2, 0)
+Vision3 = Vision(Ports.PORT20, 24, Vision5__ORANGE_FRUIT, Vision5__LIME_FRUIT, Vision5__LEMON_FRUIT)
 
 
 # Motor and Sensor Setup --------------------------------------------
@@ -58,15 +57,15 @@ def append_objects(list, tuple, type):
         for x in tuple:
             if x.width > 10 and x.height > 20:
                 list.append((x, type))
-                print(type)
+                # print(type)
     return list
 
 #Returns the vision object of the biggest fruit
 def get_biggest_fruit():
     fruit = []
-    fruit = append_objects(fruit, Vision3.take_snapshot(Vision3__LIME), "lime")
-    fruit = append_objects(fruit, Vision3.take_snapshot(Vision3__LEMON), "lemon")
-    fruit = append_objects(fruit, Vision3.take_snapshot(Vision3__ORANGE), "orange")
+    fruit = append_objects(fruit, Vision3.take_snapshot(Vision5__LIME_FRUIT), "lime")
+    fruit = append_objects(fruit, Vision3.take_snapshot(Vision5__LEMON_FRUIT), "lemon")
+    fruit = append_objects(fruit, Vision3.take_snapshot(Vision5__ORANGE_FRUIT), "orange")
 
     result = None
     if len(fruit) > 0:
@@ -79,9 +78,9 @@ def get_biggest_fruit():
 #Returns the vision object of the closest to center fruit
 def get_centered_fruit():
     fruit = []
-    fruit = append_objects(fruit, Vision3.take_snapshot(Vision3__LIME), "lime")
-    fruit = append_objects(fruit, Vision3.take_snapshot(Vision3__LEMON), "lemon")
-    fruit = append_objects(fruit, Vision3.take_snapshot(Vision3__ORANGE), "orange")
+    fruit = append_objects(fruit, Vision3.take_snapshot(Vision5__LIME_FRUIT), "lime")
+    fruit = append_objects(fruit, Vision3.take_snapshot(Vision5__LEMON_FRUIT), "lemon")
+    fruit = append_objects(fruit, Vision3.take_snapshot(Vision5__ORANGE_FRUIT), "orange")
 
     result = None
     if len(fruit) > 0:
@@ -105,7 +104,7 @@ def angle_error_deg(target, current):
 # Function Definitions -------------------------------------------------------------
 #Drive towards the biggest fruit the robot can currently see
 def find_fruit():
-    acc = 20
+    acc = 200
     while acc > 0 and not controller.buttonY.pressing():
         fruit_object = get_biggest_fruit()
 
@@ -114,12 +113,16 @@ def find_fruit():
             x_error = (fruit.centerX - RESOLUTION_WIDTH/2)*GAIN_X #right is +
             # y_error = -(fruit.centerY - RESOLUTION_HEIGHT/2 - 10)*GAIN_Y #down is +
 
-            left_motor.spin(REVERSE, 20 - x_error, PERCENT)
-            right_motor.spin(REVERSE, 20 + x_error, PERCENT)
+            left_motor.spin(FORWARD, 30 - x_error, PERCENT)
+            right_motor.spin(FORWARD, 30 + x_error, PERCENT)
             # effector_motor.spin(REVERSE, y_error, PERCENT)
 
             print("Targeting: " + fruit_object[1])
-            acc = 20
+            print("Torque: " + str(effector_motor.torque()))
+            if (effector_motor.torque() < 0.9):
+                acc = 200
+            else:
+                acc = max(acc-10, 0)
         else:
             print("No Fruit Found")
             acc = max(acc-1, 0)
@@ -150,13 +153,13 @@ def line_follow_dist_cm(dist_to_travel_cm, speed_percent):
     left_motor.reset_position()
     right_motor.reset_position()
 
-    while (abs(2*center_motor.position(RotationUnits.REV)*math.pi*WHEEL_RAD_CM)/DRIVE_GEAR_RATIO < dist_to_travel_cm):
+    while ((left_motor.position(RotationUnits.REV)+right_motor.position(RotationUnits.REV))*math.pi*WHEEL_RAD_CM*DRIVE_GEAR_RATIO < dist_to_travel_cm):
         errorL = -scale(left_line.value(), MIN_REFLECTIVITY, MAX_REFLECTIVITY)
         errorR = scale(right_line.value(), MIN_REFLECTIVITY, MAX_REFLECTIVITY)
         line_error = (errorL + errorR) * LINE_FOLLOWING_GAIN * speed_percent
 
-        left_motor.spin(FORWARD, speed_percent + line_error, PERCENT)
-        right_motor.spin(FORWARD, speed_percent - line_error, PERCENT)
+        left_motor.spin(FORWARD, speed_percent - line_error, PERCENT)
+        right_motor.spin(FORWARD, speed_percent + line_error, PERCENT)
         sleep(20)
 
     left_motor.stop()
@@ -166,24 +169,28 @@ def line_follow_dist_cm(dist_to_travel_cm, speed_percent):
 ARM_LEVELS = {
   "travel": 0,
   "tree_1": 13,
-  "tree_2": 28,
+  "tree_2": 25,
   "tree_3": 41,
   "box": 23
 }
 def set_arm(arm_level, intake_speed):
+    elbow_motor.set_stopping(BrakeType.BRAKE)
+    print("A")
     elbow_motor.spin_to_position(ARM_LEVELS[arm_level]/ARM_GEAR_RATIO, DEGREES, 100, RPM, True)
-    # elbow_motor.spin_to_position(15, DEGREES, 100, RPM, True)
-    # print(elbow_motor.position)
+    if(arm_level == "travel"):
+        elbow_motor.stop()
+        elbow_motor.set_stopping(BrakeType.COAST)
+    print("B")
     effector_motor.spin(FORWARD, intake_speed, PERCENT)
 
 # Drive forward a set distance based purely off drivetrain encoders
-def drive_forward(distance_cm, speed_percent):
+def drive_forward(dist_to_travel_cm, speed_percent):
     left_motor.reset_position()
     right_motor.reset_position()
     left_motor.spin(FORWARD, speed_percent, PERCENT)
     right_motor.spin(FORWARD, speed_percent, PERCENT)
 
-    while ((left_motor.position(RotationUnits.REV)+right_motor.position(RotationUnits.REV))*math.pi*WHEEL_RAD_CM < distance_cm):
+    while (abs((left_motor.position(RotationUnits.REV)+right_motor.position(RotationUnits.REV))*math.pi*WHEEL_RAD_CM*DRIVE_GEAR_RATIO) < dist_to_travel_cm):
         sleep(20)
 
     left_motor.stop()
@@ -211,18 +218,23 @@ def calibrate_robot():
 
 # The routine to be executed when button is pressed --------------------------------
 def auton_routine():
+    print(str(elbow_motor.temperature()))
     calibrate_robot()
-    # set_arm("travel", 0)
-    line_follow_dist_cm(100, 60)
-    # gyro_turn(90, 50)
-    # set_arm("TREE_2", 100)
-    # drive_forward(10, 40)
-    # set_arm("TREE_2", 0)
-    # find_fruit()
-    # elbow_motor.reset_position()
-    # elbow_motor.set_stopping(BrakeType.HOLD)
-    # print("TFYUGIYUUIYJU")
-    # set_arm("tree_1", 0)
+    set_arm("travel", 0)
+    line_follow_dist_cm(82, 60)
+    gyro_turn(-90, 50)
+    drive_forward(5, -10)
+    sleep(500)
+    set_arm("tree_2", 100)
+    find_fruit()
+    drive_forward(16, -20)
+    set_arm("travel", 0)
+    gyro_turn(-180, 50)
+    line_follow_dist_cm(70, 60)
+    gyro_turn(-90, 50)
+    set_arm("travel", -30)
+    sleep(1000)
+    gyro_turn(0, 50)
 
 controller.buttonB.pressed(auton_routine)
 
